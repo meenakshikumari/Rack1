4bd3244b299fdda16170bf2dbd3183eb3aae2796
meenakshikumari <meenakshi@joshsoftware.com>
2019-02-07 17:49:34 +0530

    Add routes for getusers

--- fetch_user.rb
+++ fetch_user.rb
@@ -7,7 +7,7 @@ class FetchUser
   end
 
   def handle_request(method, path, query)
-    if method == "GET"
+    if method == "GET" && path == "/getusers"
       params = Rack::Utils.parse_nested_query(query)
       db  = db_conn
       arr = []
@@ -18,7 +18,7 @@ class FetchUser
       json = { data: arr }.to_json
       get(json)
     else
-      method_not_allowed(method)
+      method_not_allowed(method, path)
     end
   end
 
@@ -26,8 +26,8 @@ class FetchUser
     [200, { "Content-Type" => "application/json" }, [json]]
   end
 
-  def method_not_allowed(method)
-    [405, {}, ["Method not allowed: #{method}"]]
+  def method_not_allowed(method, path)
+    [405, {}, ["Method not allowed for: #{method} and path #{path}"]]
   end
 
   def db_conn
