e1847f6c579d431f4044243f498fb1a0d1dcd12c
meenakshikumari <meenakshi@joshsoftware.com>
2019-02-09 18:11:25 +0530

    Code Refactor

--- fetch_user.rb
+++ fetch_user.rb
@@ -3,21 +3,41 @@ require 'sequel'
 class FetchUser
 
   def call(env)
-    handle_request(env['REQUEST_METHOD'], env['PATH_INFO'], env['QUERY_STRING'])
+    process(env['REQUEST_METHOD'], env['PATH_INFO'], env['QUERY_STRING'])
   end
 
   private
 
-    def handle_request(method, route, query)
-      if method == "GET" && route == "/getusers"
-        user_params = Rack::Utils.parse_nested_query(query)
-        user_data   = generate_response(user_params)
-        display(user_data)
-      elsif method == "GET" && route == "/"
-        login
+    def process(method, route, query)
+      if method == "GET"
+        case route
+        when "/getusers"
+          user_params = Rack::Utils.parse_nested_query(query)
+          user_data   = generate_response(user_params)
+          display(user_data)
+        when "/"
+          login
+        else
+          method_not_allowed(method, route)
+        end
       else
         method_not_allowed(method, route)
       end
+    end
+
+    def generate_response(params)
+      db  = db_conn
+      user_data = []
+
+      db[:users].select(:name, :email).where(name: "#{params["name"]}").each do |row|
+        user_data << {name: row[:name], email: row[:email]}
+      end
+      user_data << "No record of #{params["name"]} found" if user_data.empty?
+      { user_data: user_data }.to_json
+    end
+
+    def db_conn
+      Sequel.postgres(database: 'testdb', user: 'postgres')
     end
 
     def display(user_data)
@@ -31,20 +51,4 @@ class FetchUser
     def method_not_allowed(method, route)
       [405, {}, ["Method not allowed for: #{method} and route #{route}"]]
     end
-
-    def generate_response(params)
-      db  = db_conn
-      user_data = []
-
-      db[:users].select(:name, :email).where(name: "#{params["name"]}").all.each do |row|
-        user_data << {name: row[:name], email: row[:email]}
-      end
-      user_data << "No record of #{params["name"]} found" if user_data.empty?
-      { user_data: user_data }.to_json
-    end
-
-    def db_conn
-      Sequel.postgres(database: 'testdb', user: 'postgres')
-    end
-
 end
